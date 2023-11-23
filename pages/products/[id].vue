ae75e6b4c9d76d31130522917456ae927d51e58b
Trevor_143 <trevorarapu2@gmail.com>
2023-11-23 11:43:01 +0300

    working on single categories

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <h1>product - {{ id }}</h1>
+        <h1>{{ id }} products</h1>
+        <div class="productList">
+            <div class="eachProduct" v-for="item in catData" :key="item.id">
+                <div>
+                    <img :src="item.image" :alt="item.title">
+                    <h1>{{ item.title }}</h1>
+                    <p>{{ item.price }}</p>
+                </div>
+                <button>Add to cart</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -14,10 +24,38 @@ const { data } = await useFetch('/api/getSingleCategory', {
         catID: id
     }
 })
-console.log(data.value)
+const catData = ref([])
+console.log(data.value.catData)
+catData.value = data.value.catData
 
 </script>
 
 <style scoped>
+
+.productList {
+    display: grid;
+    align-items: center;
+    gap: 2rem;
+    grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
+}
+.eachProduct {
+    background-color: #f3f3f3;
+    border: 3px solid #f3f3f3;
+    min-height: 400px;
+}
+.eachProduct img {
+    width: 100%;
+    max-height: 250px;
+    min-height: 250px;
+    object-fit: cover;
+}
+.eachProduct h1 {
+    display: -webkit-box;
+    overflow: hidden;
+    line-clamp: 2;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    font-size: 1rem;
+}
 
 </style>
